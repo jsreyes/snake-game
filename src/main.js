46f82c1dbef733318b2e2036325c52f9d71bc8a6
Javier Reyes <ingjsreyesm@gmail.com>
2022-06-24 22:13:18 -0500

    :art: structure created to Phaser game

---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+const config = {
+  title: 'Snake',
+  width: 320,
+  height: 180,
+  type: Phaser.AUTO,
+  parent: 'container',
+  backgroundColor: '#f9ca24',
+  pixelArt: true,
+};
+
+new Phaser.Game(config);
